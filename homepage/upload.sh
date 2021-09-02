b29107cf3e21ec738d10ef8566787aa7728659f1
Huanle XU <xu900611@163.com>
2021-09-02 11:47:44 +0800

    Site updated at 2021-09-02 03:47:43 UTC

--- homepage/upload.sh
+++
@@ -1,4 +0,0 @@
-scp -P 5900 index.html hlxu@vipc3.ie.cuhk.edu.hk:index.html
-scp -P 5900 teaching.html hlxu@vipc3.ie.cuhk.edu.hk:teaching.html
-scp -P 5900 publication.html hlxu@vipc3.ie.cuhk.edu.hk:publication.html 
-scp -P 5900 biography.html hlxu@vipc3.ie.cuhk.edu.hk:biography.html
--- homepage/uploadlx.sh
+++
@@ -1,4 +0,0 @@
-scp index.html hlxu@lx1.ie.cuhk.edu.hk:public_html/index.html
-scp teaching.html hlxu@lx1.ie.cuhk.edu.hk:public_html/teaching.html
-scp publication.html hlxu@lx1.ie.cuhk.edu.hk:public_html/publication.html
-scp biography.html hlxu@lx1.ie.cuhk.edu.hk:public_html/biography.html
